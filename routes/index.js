66fa1c3ff0907463a18af52e0937fa20f82d2cbd
VuDuongYeuBo <116836866+VuDuongYeuBo@users.noreply.github.com>
2023-06-27 09:55:04 +0700

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const ToyModels = require('../models/ToyModels');
+const Toy2Models = require('../models/Toy2Models');
 var router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -12,12 +13,21 @@ router.get('/admin', async (req, res) => {
   var total = await ToyModels.count();
   res.render('admin', { toys : toys , total : total })
 })
+router.get('/admin1', async (req, res) => {
+  var toys2 = await Toy2Models.find({});
+  var total = await Toy2Models.count();
+  res.render('admin1', { toys2 : toys2 , total : total })
+})
 router.get('/home', function(req, res, next) {
   res.render('home');
 });
 router.get('/list', async (req, res) => {
   var toys = await ToyModels.find({});
   res.render('list', { toys: toys });
+})
+router.get('/list1', async (req, res) => {
+  var toys2 = await Toy2Models.find({});
+  res.render('list1', { toys2: toys2 });
 })
 
 router.get('/delete/:id', async(req, res) => {
@@ -27,6 +37,13 @@ router.get('/delete/:id', async(req, res) => {
 
   res.redirect('/admin');
 })
+router.get('/delete1/:id', async(req, res) => {
+  await Toy2Models.findByIdAndDelete(req.params.id)
+  .then(() => { console.log ('Delete succeed !')})
+  .catch((err) => { console.log ('Delete failed !')});
+
+  res.redirect('/admin1');
+})
 
 router.get('/drop', async(req, res) => {
   await ToyModels.deleteMany({})
@@ -35,6 +52,12 @@ router.get('/drop', async(req, res) => {
   res.redirect('/admin');
 })
 
+router.get('/drop', async(req, res) => {
+  await ToyModels.deleteMany({})
+  .then(() => { console.log ('Delete all succeed !')});
+  
+  res.redirect('/admin1');
+})
 router.post('/order', async (req, res) => {
   var id = req.body.id;
   var toys = await ToyModels.findById(id);
@@ -55,6 +78,16 @@ router.post('/add', async (req, res) => {
   res.redirect('/admin');
 })
 
+router.get('/add1', (req, res) => {
+  res.render('add1');
+})
+
+router.post('/add1', async (req, res) => {
+  var toys2 = req.body;
+  await Toy2Models.create(toys2)
+  .then(() => { console.log ('Add new toy succeed !')});
+  res.redirect('/admin1');
+})
 router.get('/edit/:id', async (req, res) => {
   var toys = await ToyModels.findById(req.params.id);
   res.render('edit', { toys : toys});
@@ -62,9 +95,22 @@ router.get('/edit/:id', async (req, res) => {
 
 router.post('/edit/:id', async (req, res) => {
   var id = req.params.id;
-  await ToyModels.findByIdAndUpdate(id)
-  .then(() => { console.log('Edit mobile succeed !') });
+  var updatedData1=req.body;
+  await ToyModels.findByIdAndUpdate(id,updatedData1)
+     .then(() => { console.log('Edit toy succeed!') });
   res.redirect('/admin');
+})
+router.get('/edit1/:id', async (req, res) => {
+  var toys2 = await Toy2Models.findById(req.params.id);
+  res.render('edit1', { toys2 : toys2});
+})
+
+router.post('/edit1/:id', async (req, res) => {
+  var id = req.params.id;
+  var updatedData=req.body;
+  await Toy2Models.findByIdAndUpdate(id,updatedData)
+     .then(() => { console.log('Edit toy succeed!') });
+  res.redirect('/admin1');
 })
 
 module.exports = router;
